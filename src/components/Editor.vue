74a703042c91eaaa9b267999df4ac3eed0e71aaa
youcune <you@nakanishi.email>
2018-11-10 16:14:38 +0900

    5章まで

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -4,7 +4,15 @@
     <span>{{ user.displayName }}</span>
     <button @click="logout">ログアウト</button>
     <div class="editorWrapper">
-      <textarea class="markdown" v-model="markdown"></textarea>
+      <div class="memoListWrapper">
+        <div class="memoList" v-for="(memo, index) in memos" @click="selectMemo(index)" :data-selected="index == selectedIndex">
+          <p class="memoTitle">{{ displayTitle(memo.markdown) }}</p>
+        </div>
+        <button class="addMemoBtn" @click="addMemo">メモの追加</button>
+        <button class="deleteMemoBtn" v-if="memos.length > 1" @click="deleteMemo">選択中のメモの削除</button>
+        <button class="saveMemosBtn" @click="saveMemos">メモの保存</button>
+      </div>
+      <textarea class="markdown" v-model="memos[selectedIndex].markdown"></textarea>
       <div class="preview" v-html="preview()"></div>
     </div>
   </div>
@@ -17,24 +25,66 @@ export default {
   props: ['user'],
   data() {
     return {
-      markdown: ''
+      memos: [ { markdown: ''} ],
+      selectedIndex: 0
     };
+  },
+  created: function() {
+    firebase.database().ref('memos/' + this.user.uid).once('value').then(result => {
+      if (result.val()) { this.memos = result.val(); }
+    });
   },
   methods: {
     logout: () => firebase.auth().signOut(),
-    preview: function(){ return marked(this.markdown); }
+    addMemo: function() {
+      this.memos.push({ markdown: '無題のメモ' });
+    },
+    selectMemo: function(index) {
+      this.selectedIndex = index;
+    },
+    deleteMemo: function() {
+      this.memos.splice(this.selectedIndex, 1);
+    },
+    saveMemos: function() {
+      firebase.database().ref('memos/' + this.user.uid).set(this.memos);
+    },
+    preview: function(){ return marked(this.memos[this.selectedIndex].markdown); },
+    displayTitle: (text) => { return text.split(/\n/)[0]; }
   }
 };
 </script>
 <style lang="sass" scoped>
-.editorWrapper
-  display: flex
+.memoListWrapper
+  width: 19%
+  float: left
+  border-top: 1px solid #000
+
+.memoList
+  padding: 10px
+  box-sizing: border-box
+  text-align: left;
+  border-bottom: 1px solid #000
+  &:nth-child(even)
+    background-color: #ccc
+  &[data-selected="true"]
+    background-color: #ccf
+
+.memoTitle
+  height: 1.5em
+  margin: 0
+  white-space: nowrap
+  overflow: hidden
+
+.addMemoBtn
+  margin-top: 20px
 
 .markdown
-  width: 50%
+  float: left
+  width: 40%
   height: 500px
 
 .preview
-  width: 50%
+  float: left
+  width: 40%
   text-align: left
 </style>
