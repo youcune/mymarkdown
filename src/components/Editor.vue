a36ce8234ddc6656f20a44d8b7778edc2d9e606c
youcune <you@nakanishi.email>
2018-11-10 15:20:52 +0900

    5.1まで

---
+++ src/components/Editor.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="editor">
+    <h1>エディター画面</h1>
+    <span>{{ user.displayName }}</span>
+    <button @click="logout">ログアウト</button>
+    <div class="editorWrapper">
+      <textarea class="markdown" v-model="markdown"></textarea>
+      <div class="preview" v-html="preview()"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+import marked from 'marked';
+export default {
+  name: 'editor',
+  props: ['user'],
+  data() {
+    return {
+      markdown: ''
+    };
+  },
+  methods: {
+    logout: () => firebase.auth().signOut(),
+    preview: function(){ return marked(this.markdown); }
+  }
+};
+</script>
+<style lang="sass" scoped>
+.editorWrapper
+  display: flex
+
+.markdown
+  width: 50%
+  height: 500px
+
+.preview
+  width: 50%
+  text-align: left
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="home">
+    <h1>{{ msg }}</h1>
+    <button @click="googleLogin">Googleアカウントでログイン</button>
+  </div>
+</template>
+<script>
+export default {
+  name: 'home',
+  data() {
+    return{
+      msg: 'Welcome to MyMarkdown'
+    };
+  },
+  methods: {
+    googleLogin: () =>
+      firebase.auth().signInWithRedirect(new firebase.auth.GoogleAuthProvider())
+  }
+};
+</script>
